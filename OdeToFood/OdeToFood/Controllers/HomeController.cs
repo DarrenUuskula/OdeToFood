e772912164044a463d9d206e82669de7a30ebf99
DarrenUuskula <115153527+DarrenUuskula@users.noreply.github.com>
2023-01-22 06:56:08 +0200

    Add design (background and logo)

--- OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
@@ -22,18 +27,18 @@ namespace OdeToFood.Controllers
 		{
 			return View();
 		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
 		public IActionResult About()
 		{
 			var model = new AboutModel();
 			model.Name = "Darren";
 			model.Location = "Tallinn, Estonia";
 			return View(model);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 	}
 }
