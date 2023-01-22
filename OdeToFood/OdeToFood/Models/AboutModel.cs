c5798dae2c95bb680b36efcbf21801e1837651a2
DarrenUuskula <115153527+DarrenUuskula@users.noreply.github.com>
2023-01-22 06:58:07 +0200

    Add route message to index so I can see routedata contents

--- OdeToFood/OdeToFood/Models/AboutModel.cs
+++ OdeToFood/OdeToFood/Models/AboutModel.cs
@@ -1,6 +1,13 @@
-﻿namespace OdeToFood.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Models
 {
 	public class AboutModel
 	{
+		public string Name { get; set; }
+		public string Location { get; set; }
 	}
 }
